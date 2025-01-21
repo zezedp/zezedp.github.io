3bef2d6891302d67b4f874b34b992d631c9be032
whitepaper233 <1423634265@qq.com>
2025-01-21 15:44:49 +0800

    Fix: Update swup containers and prevent duplicate style elements in banner

--- astro.config.mjs
+++ astro.config.mjs
@@ -25,7 +25,7 @@ export default defineConfig({
     icon(),
     swup({
       theme: false,
-      containers: ["main", ".banner-inner"],
+      containers: ["main", "footer", ".banner-inner"],
       smoothScrolling: true,
       progress: true,
       cache: true,
